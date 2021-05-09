39b869c2293bab01409a52ccb17807fe17ed4f6f
Hbeba115 <habiba.mahmoud151@gmail>
2021-05-09 06:52:56 +0200

    Editing Det.h file, Adding Det.c file

---
+++ Det.c
@@ -0,0 +1,14 @@
+/********************************************************************************************s
+ * File Name	:	Det.c
+ * Version		:	1.0.0
+ * Created on  	: 	8/05/2021
+ * Author      	: 	Nihal - Habiba - Nour
+ ********************************************************************************************/
+
+#include "Det.h"
+
+Std_ReturnType Det_ReportError(uint16 ModuleId,uint8 InstanceId,uint8 ApiId,uint8 ErrorId){
+	while(1){
+
+	}
+}
--- Det.h
+++ Det.h
@@ -11,6 +11,23 @@
 
 #include"Std_Types.h"
 
+
+
+#define DET_VENDOR_ID                    (uint16)0x0026
+
+#define	DET_MODULE_ID					 (uint8)120
+
+#define	DET_INSTANCE_ID		             (uint8)1
+
+#define DET_SW_MAJOR_VERSION             (uint8)1
+#define DET_SW_MINOR_VERSION             (uint8)0
+#define DET_SW_PATCH_VERSION             (uint8)0
+
+#define DET_AR_RELEASE_MAJOR_VERSION     (uint8)1
+#define DET_AR_RELEASE_MINOR_VERSION     (uint8)0
+#define DET_AR_RELEASE_PATCH_VERSION     (uint8)0
+
+
 Std_ReturnType Det_ReportError(uint16 ModuleId,uint8 InstanceId,uint8 ApiId,uint8 ErrorId);
 
 
